2bed3e1e0b917d4eb5e8348e54517b665d4525b0
Niphan Sethi <sethiniphan@gmail.com>
2020-11-25 17:54:08 +0700

    completed application with all functionality

---
+++ MixtapeCreatorApp.py
@@ -0,0 +1,197 @@
+try:
+    import tkinter as tk
+    import os
+    from urllib import request, parse
+    from pytube import YouTube
+    from moviepy import editor
+
+except Exception as e:
+    print("Please install the required modules to execute the program: {}".format(e))
+    exit()
+
+##################################################################################################################
+
+class MixtapeCreator:
+
+    @staticmethod
+    def read_songs_file(path_to_songs_list, mixtape_dir, state, root, total_songs):
+        file = open(path_to_songs_list, "r")
+        unsuccessful_downloads = MixtapeCreator.create_mixtape(file, mixtape_dir, state, root, total_songs)
+        file.close()
+        return unsuccessful_downloads
+    
+    @staticmethod
+    def determine_url(line):
+        query_string = parse.urlencode({"search_query" : line})
+        html_content = request.urlopen("https://www.youtube.com/results?" + query_string)
+        html_content = html_content.read().decode()
+        index = html_content.find("/watch?v=")
+        video_url = "https://www.youtube.com"
+        for i in range(index, index + 20):
+            video_url += html_content[i]
+        return video_url
+    
+    @staticmethod
+    def determine_filename(line):
+        invalid_chars = "_*<>\'?\n%|:\"}{/\\"
+        temp = line
+        filename = ""
+
+        for char in temp:
+            if char in invalid_chars:
+                temp = temp.replace(char, '')
+
+        for i in range(0, len(temp)):
+            if i == 0:
+                filename = temp[i].upper() if temp[i].islower() else temp[i]
+            else:
+                if temp[i - 1] == ' ' and temp[i].isalpha() and temp[i].islower():
+                    filename += temp[i].upper()
+                else:
+                    filename += temp[i]
+        return filename
+    
+    @staticmethod
+    def download_vid(mixtape_dir, audio_filename, video_url):
+        print(audio_filename)
+        print(video_url)
+        yt = YouTube(video_url)
+        audio = yt.streams.filter(only_audio = True).first()
+        audio.download(output_path = mixtape_dir, filename = audio_filename)
+    
+    @staticmethod
+    def convert_to_mp3(mixtape_dir, audio_filename):
+            mp4_dir = mixtape_dir + '/' + audio_filename + ".mp4"
+            mp3_dir = mp4_dir.replace(".mp4", ".mp3")
+            audioClip = editor.AudioFileClip(mp4_dir)
+            audioClip.write_audiofile(mp3_dir)
+            audioClip.close()
+            os.remove(mp4_dir)
+    
+    @staticmethod
+    def update(root, state, message):
+        state.configure(text = message)
+        root.update()
+
+    @staticmethod
+    def create_mixtape(file, mixtape_dir, state, root, total_songs):
+        num_downloaded = 0
+        unsuccessful_downloads = []
+        # iterate through all songs listed in the file
+        for line in file:
+            # Skip empty lines in file
+            if line == '\n' or line == "":
+                continue
+            
+            try:
+                if ":" not in line:
+                    MixtapeCreator.update(root, state, "Error :     Line {} incorrectly formatted, skipping download...".format(num_downloaded + 1))
+                    continue
+
+                # Determine filename of audio
+                audio_filename = MixtapeCreator.determine_filename(line)
+                audio_filename = audio_filename.replace("  ", "-")
+                # Determine url of youtube video
+                video_url = MixtapeCreator.determine_url(line.replace(':', '-'))
+                # Download audio of Youtube Video
+                MixtapeCreator.download_vid(mixtape_dir, audio_filename, video_url)
+                # Convert mp4 file to mp3 file and remove mp4 file from corresponding directory
+                MixtapeCreator.convert_to_mp3(mixtape_dir, audio_filename)
+                # Increment counter by 1 
+                num_downloaded += 1
+                # Update GUI to show number of songs successfully downloaded
+                MixtapeCreator.update(root, state, "Please Wait :     Creating... ({}/{} songs successfully downloaded)".format(num_downloaded, total_songs))
+            except Exception:
+                unsuccessful_downloads.append(line)
+
+        return unsuccessful_downloads
+            
+            
+    
+    
+##################################################################################################################
+
+class InputValidator:
+    
+    def __init__(self, songs_list_path, save_to_dir, mixtape_title):
+        self.songs_list_path = songs_list_path
+        self.save_to_dir = save_to_dir
+        self.mixtape_title = mixtape_title
+        self.determine_mixtape_path()
+    
+    # This method is invoked as the callback for pressing the "Create Mixtape" button
+    # Instead of creating a new object for each mainloop iteration, an object is only created 
+    # upon the click of the "Create Mixtape" button. 
+    @staticmethod
+    def create_instance(songs_list_path, save_to_dir, mixtape_title):
+        return InputValidator(songs_list_path, save_to_dir, mixtape_title)
+
+    def validate_input(self, root, state, songs_list_entry, dir_entry, title_entry):
+        if self.has_empty_fields():
+            state.configure(text = "Please fill all fields")
+        elif not self.is_valid_path(self.songs_list_path.user_entry.get()):
+            state.configure(text = "Invalid field entry : the specified path to the songs list does not exist")
+        elif not self.is_valid_path(self.save_to_dir.user_entry.get()):
+            state.configure(text = "Invalid field entry : the specified save-to directory does not exist")
+        elif self.is_valid_path(self.mixtape_path):
+            state.configure(text = "Invalid field entry : mixtape title conflicts with another directory in the path specified")
+        else:
+            try:
+                total_songs = self.count_num_songs(self.songs_list_path.user_entry.get())
+                os.mkdir(self.mixtape_path)
+                MixtapeCreator.update(root, state, "Please Wait :     Creating... (0/{} songs successfully downloaded)".format(total_songs))
+                unsuccessful_downloads = MixtapeCreator.read_songs_file(self.songs_list_path.user_entry.get(), self.mixtape_path, state, root, total_songs)
+                self.determine_final_state(root, state, unsuccessful_downloads, total_songs)
+            except Exception:
+                state.configure(text = "Invalid field entry : mixtape title must not contain special characters")
+    
+    # update and display final state of the application based on output
+    def determine_final_state(self, root, state, unsuccessful_downloads, total_songs):
+        if len(unsuccessful_downloads) == 0:
+            print("Your Mixtape Has Successfully Been Created!")
+            MixtapeCreator.update(root, state, "Completed : All songs successfully downloaded!")
+        else:
+            if len(unsuccessful_downloads) == total_songs:
+                print("Poor Internet Connection Prevented the Application from Executing!")
+                MixtapeCreator.update(root, state, "Complete Failure : Poor internet connection prevented downloads")
+                os.rmdir(self.mixtape_path)
+            else:
+                print("Your Mixtape Has Been Created!")
+                MixtapeCreator.update(root, state, "Completed : Partial success (please view the \"Unsuccessful_Downloads.txt\"  file in the mixtape directory)")
+                # create file with unsuccessful downloads in directory
+                with open(self.mixtape_path + "/Unsuccessful_Downloads.txt", "w") as filehandle:
+                    for song in unsuccessful_downloads:
+                        filehandle.write("%s" % song)
+        
+    def determine_mixtape_path(self):
+        self.mixtape_path = self.save_to_dir.user_entry.get() + '/' + self.mixtape_title.user_entry.get()
+
+    def has_empty_fields(self):
+        if self.songs_list_path.user_entry.cget("state") == tk.DISABLED or \
+           self.save_to_dir.user_entry.cget("state") == tk.DISABLED or \
+           self.mixtape_title.user_entry.cget("state") == tk.DISABLED:
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def is_valid_path(path_entered):
+        if os.path.exists(path_entered):
+            return True
+        else:
+            return False
+    
+    def count_num_songs(self, file_path):
+        num_songs = 0
+        file = open(file_path, "r")
+
+        for line in file:
+            if line == '\n' or line == "":
+                continue
+            num_songs += 1
+
+        file.close()
+        return num_songs
+
+
+##################################################################################################################
---
+++ MixtapeCreatorGUI.py
@@ -0,0 +1,214 @@
+try: 
+    import tkinter as tk
+    from tkinter import filedialog
+    from PIL import ImageTk, Image
+    import MixtapeCreatorApp
+
+except Exception as e:
+    print("Please install the required modules to execute the program: {}".format(e))
+    exit()
+
+##################################################################################################################
+
+# This class represents all the attributes and methods needed to create and format the GUI
+
+class MixtapeCreatorGUI:
+
+    def __init__(self, window_dim, bg_colour, win_height_offset):
+        self.window_dim = window_dim
+        self.bg_colour = bg_colour
+        self.win_height_offset = win_height_offset
+        self.root = tk.Tk()
+    
+    def main(self):
+        bg_canvas = self.set_initial_params()
+        songs_list_path = EntryBox(0.233, 0.26, bg_canvas, 44, "white", 
+        "Enter File Path to Songs List", ("Calibri", 11), tk.LEFT, True, "file")
+        
+        save_to_dir = EntryBox(0.233, 0.3, bg_canvas, 44, "white", 
+        "Save To Directory...", ("Calibri", 11), tk.LEFT, True, "dir")
+        
+        mixtape_img = ImageTk.PhotoImage(self.load_mixtape_image())
+        bg_canvas.create_image(353, 388, anchor = tk.CENTER, image = mixtape_img)
+
+        mixtape_title = EntryBox(0.273, 0.43, bg_canvas, 32, "#e7e5cc", "Enter Mixtape Title", 
+        ("Permanent Marker", 11), tk.CENTER, False, "none")
+
+        state = tk.Label(bg_canvas, text = "Please fill all fields", bg = self.bg_colour, fg = "#9c9c9c", font = ("Calibri", 8))
+        state.place(relx = 0.5, rely = 0.87, anchor = tk.CENTER)
+
+        self.create_main_btns(self.root, bg_canvas, songs_list_path, save_to_dir, mixtape_title, state)
+        self.root.mainloop()
+
+    # This method sets the GUI window position to the center of the screen
+    def set_window_position(self):
+        position_right = int(self.root.winfo_screenwidth()/2 - self.window_dim/2)
+        position_down = int(self.root.winfo_screenheight()/2 - self.window_dim/2) - self.win_height_offset
+        self.root.geometry("{}x{}+{}+{}".format(self.window_dim, self.window_dim, position_right, position_down))
+
+    # This method sets the title of the program and displays it on the GUI
+    def set_gui_title(self, canvas_to_link):
+        gui_header = tk.Label(canvas_to_link, text = "MIXTAPE CREATOR", 
+            font = ("Lato", 30), bg = self.bg_colour, fg = "white")
+        gui_header.place(relx = 0.5, rely = 0.15, anchor = tk.CENTER)
+
+    # This method initializes the contents of the GUI
+    def set_initial_params(self):
+        # do not allow window to be resized
+        self.root.resizable(False, False)
+        # set GUI description
+        self.root.title("Mixtape Creator - Version 1 (By Niphan Sethi)")
+        # position window to open at center of screen
+        self.set_window_position()
+        # create a canvas and pack it to root (used as background of GUI)
+        canvas = tk.Canvas(self.root, height = self.window_dim , width = self.window_dim, 
+        bg = self.bg_colour, highlightcolor = "white")
+        canvas.pack()
+        # set and display title on canvas
+        self.set_gui_title(canvas)
+        return canvas
+    
+    def create_main_btns(self, root, link_to, songs_list_entry, dir_entry, title_entry, state):
+
+        button_border = tk.Frame(link_to, width = 380, height = 30, bg = "#9c9c9c")
+        button_border.place(relx = 0.233, rely = 0.775, anchor = tk.NW)
+
+        start_btn = tk.Button(button_border, text = "Create Mixtape", font = ("Calibri", 9), padx = 49, 
+        command = lambda : MixtapeCreatorApp.InputValidator.create_instance(songs_list_entry, dir_entry, title_entry).validate_input(root, state, songs_list_entry, dir_entry, title_entry))
+        start_btn.place(relx = 0.998, rely = 0.1, anchor = tk.NE)
+
+        reset_btn = tk.Button(button_border, text = "Reset Fields", font = ("Calibri", 9), padx = 57, 
+        command = lambda : self.reset_all(songs_list_entry, dir_entry, title_entry, state))
+
+        reset_btn.place(relx = 0.007, rely = 0.1, anchor = tk.NW)
+
+    
+    # This method loads the image of the mixtape onto the GUI
+    @staticmethod
+    def load_mixtape_image():
+        load_img = Image.open("res/mixtape_image.png")
+        load_img = load_img.resize((382, 250), Image.ANTIALIAS)
+        return load_img
+    
+    # This method resets all of the entry boxes to the default text
+    @staticmethod
+    def reset_all(songs_list_entry, dir_entry, title_entry, state):
+        songs_list_entry.set_default_text() 
+        dir_entry.set_default_text()
+        title_entry.set_default_text()
+        # indicate to users to fill all fields before proceeding with mixtape creation
+        state.configure(text = "Please fill all fields")
+
+##################################################################################################################
+
+# This class represents the contents of an entry object. It contains all of the relevant attributes 
+# and methods to position and style the entry box
+
+class EntryBox:
+
+    def __init__(self, x_pos, y_pos, link_to, box_width, box_colour, default_text, 
+                text_font, text_anchor, include_browse, browse_type):
+        self.relx_pos = x_pos
+        self.rely_pos = y_pos
+        self.link_to = link_to
+        self.box_width = box_width
+        self.box_colour = box_colour
+        self.default_text = default_text
+        self.text_font = text_font
+        self.text_anchor = text_anchor
+        # use the specified attributes to create an entry box
+        self.create_user_entry()
+        # use a flag variable to determine whether an entry object needs a corresponding browse button
+        if include_browse:
+            self.browse_button = BrowseButton(link_to, y_pos, self.user_entry, self, browse_type)
+
+    # This method deletes the default text in the entry box (called when the entry box is 
+    # clicked by the user)
+    def enter_text(self, user_entry):
+        # change the state of the entry box from DISABLED to NORMAL
+        user_entry.configure(state = tk.NORMAL)
+        # delete default text
+        user_entry.delete(0, tk.END)
+
+    # This method allows the user to enter text into the entry box when clicked
+    def on_click(self, event):
+        # if user has not entered any text into the entry box, delete default text and 
+        # change state to NORMAL        
+        if self.user_entry.cget("state") != tk.NORMAL:
+            self.enter_text(self.user_entry)
+
+    # This method is reponsible for setting the default text into the entry box
+    def set_default_text(self):
+        # delete all existing text (if any)
+        self.user_entry.delete(0, tk.END)
+        # insert default text
+        self.user_entry.insert(0, self.default_text)
+        # set state as DISABLED and set default text with required font parameters
+        self.user_entry.configure(state = tk.DISABLED, font = self.text_font)
+
+    # This method resets the entry box back to its default configuration
+    def reset_user_entry(self, event):
+        if len(self.user_entry.get()) == 0 and self.user_entry.cget("state") == tk.NORMAL:
+            self.set_default_text()
+
+    # This method is responsible for creating the entry box based on the specified attributes
+    def create_user_entry(self):
+        self.user_entry = tk.Entry(self.link_to, width = self.box_width, justify = self.text_anchor)
+        self.user_entry.place(relx = self.relx_pos, rely = self.rely_pos)
+        self.user_entry.configure(disabledbackground = self.box_colour, bg = self.box_colour, relief = tk.FLAT)
+        self.set_default_text()
+        # user can only type in entry box when it is clicked
+        self.user_entry.bind("<Button>", self.on_click)
+        # if user clicks away from entry box and entry box empty, reset to its default text
+        self.user_entry.bind("<FocusOut>", self.reset_user_entry)
+
+##################################################################################################################
+
+# This class represents the corresponding browse button to an entry box (a user can browse 
+# for the songs list text file and a directory to store the mixtape in)
+
+class BrowseButton:
+
+    def __init__(self, link_to, y_pos, display_on, user_entry_object, browse_type):
+        self.link_to = link_to
+        self.rely_pos = y_pos
+        self.create_button(user_entry_object, display_on, browse_type)
+    
+    # This method creates the browse button according to the attributes specified
+    def create_button(self, user_entry_object, user_entry, browse_type):
+        self.browse_button = tk.Button(self.link_to, text = "Browse", 
+                             font = ("Calibri", 9), padx = 7, pady = 0.4, 
+                             command = self.determine_callback(user_entry_object, user_entry, browse_type))
+        self.browse_button.place(relx = 0.69, rely = self.rely_pos)
+    
+    # This method determines whether a file path or directory path is needed for the corresponding entry box
+    def determine_callback(self, user_entry_object, user_entry, browse_type):
+        # prompt for file
+        if browse_type == "file":
+            return lambda : self.select_file(user_entry_object, user_entry)
+        # prompt for directory
+        else:
+            return lambda : self.select_dir(user_entry_object, user_entry)
+    
+    # This method allows the user to browse for a text file path
+    def select_file(self, user_entry_object, user_entry):
+        filename = tk.filedialog.askopenfilename(initialdir = "/", title = "Select File", 
+                   filetypes = [("Text Document", "*.txt")])
+        # if a file has been selected, insert file path into entry box
+        if filename:
+            user_entry_object.enter_text(user_entry)
+            user_entry.insert(0, filename)
+
+    # This method allows the user to browse to a specific directory
+    def select_dir(self, user_entry_object, user_entry):
+        selected_directory = tk.filedialog.askdirectory()
+        # if a directory has been selected, insert into entry box
+        if selected_directory:
+            user_entry_object.enter_text(user_entry)
+            user_entry.insert(0, selected_directory)
+
+##################################################################################################################
+
+if __name__ == "__main__":
+    # Execute program
+    MixtapeCreatorGUI(700, "#263D42", 20).main()
